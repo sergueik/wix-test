2c86ab704ac7ad5d58aeba2d15d9aa8958030b51
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-06-28 21:54:37 -0400

    fixed SourceCustomAction

---
+++ WixDataBase.CustomAction/build.cmd
@@ -0,0 +1,5 @@
+REM 
+
+msbuild.exe /property:WixSdkPath=c:\wix\sdk\ /property:WixTasksPath=c:\wix\WixTasks.dll /property:WixCATargetsPath=c:\wix\SDK\wix.ca.targets /property:WixTargetsPath=c:\wix\wix.targets /property:WixTasksPath=c:\wix\WixTasks.dll /property:WixToolPath=c:\wix\ WixDatabase.CustomAction.csproj
+
+
